878feed3edacaffbd7036ce0fe74342e83a01c61
Vidya A. Surendran <Vidya.Surendran@marlabs.com>
2018-07-09 13:04:49 +0530

    Initial COmmit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">calculator
+      </div>
+    );
+  }
+}
+
+export default App;
